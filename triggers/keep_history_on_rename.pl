f71642100da7b37ca725d41321c1098f68fee7b6
Jens Brejner <jbrejner@praqma.net>
2011-08-17 22:26:08 +0200

    Changed loop

--- triggers/keep_history_on_rename.pl
+++ triggers/keep_history_on_rename.pl
@@ -1,27 +1,12 @@
 require 5.000;
 use strict;
 
-BEGIN {
-
-	# Ensure that the view-private file will get named back on rejection.
-	END {
-		rename( "$ENV{CLEARCASE_PN}.mkelem", $ENV{CLEARCASE_PN} )
-		  if $? && !-e $ENV{CLEARCASE_PN} && -e "$ENV{CLEARCASE_PN}.mkelem";
-	}
-}
-
 #Getting the script dir
 our ( $Scriptdir, $Scriptfile );
 
 BEGIN {
-	if ( __FILE__ =~ /(.*[\/\\])(.*)$/ ) {
-		$Scriptdir  = $1;
-		$Scriptfile = $2;
-	}
-	else {
-		$Scriptdir  = "";
-		$Scriptfile = __FILE__;
-	}
+    use File::Basename;
+    ( $Scriptfile, $Scriptdir ) = fileparse($0);
 }
 use File::Basename;
 use lib $Scriptdir. "..";
@@ -31,22 +16,16 @@ use praqma::trigger_helper;
 #Required if you call trigger_helper->enable_install
 our $TRIGGER_NAME   = "ACC_RENAME_HISTORY";
 our %install_params = (
-	"name"     => $TRIGGER_NAME,    # The name og the trigger
-	"mktrtype" =>
-	  "-postop lnname -element -all",    # The stripped-down mktrtype command
-	"supports" => "bccvob,ucmvob"
-	,    # csv list of generic and/or custom VOB types (case insensetive)
+    "name"     => $TRIGGER_NAME,                     # The name og the trigger
+    "mktrtype" => "-postop lnname -element -all",    # The stripped-down mktrtype command
+    "supports" => "bccvob,ucmvob",                   # csv list of generic and/or custom VOB types (case insensetive)
 );
 
 # File version
 our $VERSION  = "0.1";
 our $REVISION = "1";
-my $verbose_mode = 1
-  ; # Setting the verbose mode to 1 will print the logging information to STDOUT/ERROUT ...even it the log-file isn't enabled
-my $debug_on =
-  defined( $ENV{'CLEARCASE_TRIGGER_DEBUG'} )
-  ? $ENV{'CLEARCASE_TRIGGER_DEBUG'}
-  : undef;
+my $verbose_mode = 1;    # Setting the verbose mode to 1 will print the logging information to STDOUT/ERROUT ...even it the log-file isn't enabled
+my $debug_on = defined( $ENV{'CLEARCASE_TRIGGER_DEBUG'} ) ? $ENV{'CLEARCASE_TRIGGER_DEBUG'} : undef;
 
 # Header and revision history
 our $header = <<ENDHEADER;
@@ -55,7 +34,7 @@ our $header = <<ENDHEADER;
 #     This script is intended as ClearCase trigger script for the
 #     $TRIGGER_NAME trigger.
 #
-#     The trigger adds history to renamed elements
+#     The trigger maintains rename history to renamed elements and parent folder
 #
 #     This script supports self-install (execute with the -install
 #     switch to learn more).
@@ -75,7 +54,8 @@ our $revision = <<ENDREVISION;
 DATE        EDITOR         		NOTE
 ----------  ------------   		----------------------------------------------
 
-2011-08-04  Margit Bennetzen	Script created (based on evil twin)
+2011-08-04  Margit Bennetzen	Script created
+2011-08-17  Jens Brejner        Praqmatized
 
 ------------------------   		----------------------------------------------
 
@@ -83,13 +63,11 @@ ENDREVISION
 
 #Enable the features in trigger_helper
 our $thelp = trigger_helper->new;
-$thelp->enable_install( \%install_params )
-  ;    #Pass a reference to the install-options
+$thelp->enable_install( \%install_params );    #Pass a reference to the install-options
 $thelp->require_trigger_context;
 
 # Look for semaphore, respecting a local semaphore path via env. var.
-our $semaphore_status =
-  $thelp->enable_semaphore_backdoor( $ENV{'CLEARCASE_USE_LOCAL_SEMAPHORE'} );
+our $semaphore_status = $thelp->enable_semaphore_backdoor( $ENV{'CLEARCASE_USE_LOCAL_SEMAPHORE'} );
 
 # Initiate logging.
 our $log = scriptlog->new;
@@ -101,44 +79,69 @@ $log->enable(1);
 #Define either environment variabel CLEARCASE_TRIGGER_VERBOSE=1 or SCRIPTLOG_VERBOSE=1 to start printing to STDOUT
 $log->set_verbose($verbose_mode);
 our $logfile = $log->get_logfile();
-($logfile)
-  && $log->information("logfile is: $logfile\n")
-  ;    # Logfile is null if logging isn't enabled.
+($logfile) && $log->information("logfile is: $logfile\n");    # Logfile is null if logging isn't enabled.
 ($logfile) && $log->information($semaphore_status);
 
-#my $length = length($ENV{CLEARCASE_PN2});
-#($logfile) && $log->information("length:"+$length);
-# ($logfile) &&
-$log->dump_ccvars
-  ;    # Run this statement to have the trigger dump the CLEARCASE variables
+# Run this statement to have the trigger dump the CLEARCASE variables
+($logfile) && $log->dump_ccvars;
 
-#Finds renamed files
-my $path  = dirname( $ENV{CLEARCASE_PN2} );
-my @reply = `cleartool diff -pre  $path 2>&1`;
+# Main:
+# Continue only if operation type is what we are intended for..
+if ( lc( $ENV{CLEARCASE_OP_KIND} ) eq "lnname" ) {
 
-my @result;
-my $on = 0;
-foreach (@reply) {
+    my @result; # Interesting output files
+    my $on = 0; # 
+    my $parentfolder = dirname( $ENV{CLEARCASE_PN2} );
+    my @diffoutput   = `cleartool diff -pre "$parentfolder" 2>&1`;
 
-	if ( $on == 0 ) {
-		if ( $_ =~ /^-------------/ ) {
-			$log->information($_);
-			$on = 1;
-			push( @result, $_ );
-		}
-	}
-	else {
+    foreach (@diffoutput) {
+                    if ( $_ =~ /^-------------/ ) {
+                ($logfile) && $log->information("Found line containing: [$_]");
+                $on = 1;
+        if ( $on eq 0 ) {
+            if ( $_ =~ /^-------------/ ) {
+                ($logfile) && $log->information("Found line containing: [$_]");
+                $on = 1;
+                push( @result, $_ );
+            }
+        }
+        else {
 
-		$on = 0;
-		push( @result, $_ );
-	}
+            $on = 0;
+            push( @result, $_ );
+        }
 
-}
+    }
 
-foreach (@result) {
-	#use `cleartool chevent -c "added comment" . ` search for filename between | and whitespace 
-	$log->information($_);
+    foreach (@result) {
+
+        #use `cleartool chevent -c "added comment" . ` search for filename between | and whitespace
+        $log->information($_);
+    }
 }
 __END__
+
+my ( $index, $thislast );
+$index = 0;
+while ( $index < @diffoutput ) {
+ if ( $diffoutput[$index] =~ m/-----\[ renamed to \]-----/i ) {
+  ( my $oldname = $diffoutput[ $index + 1 ] ) =~ s/(..)(.*)(.\s+--\d+.*)/$2/;
+  ( my $newname = $diffoutput[ $index + 3 ] ) =~ s/(..)(.*)(.\s+--\d+.*)/$2/;
+  print "Element [$newname] previously named [$oldname]\n";
+  $index = $index + 4;
+  next;
+ }
+ elsif ( $diffoutput[$index] =~ m/-----\[ added \]-----/i ) {
+  ( my $addedname = $diffoutput[ $index + 1 ] ) =~ s/(..)(.*)(.\s+--\d+.*)/$2/;
+  print "New Element [$addedname]\n";
+  $index = $index + 2;
+  next;
+ }
+ else {
+
+  # print "\$index is $index  in $diffoutput[$index]\n ";
+  $index++;
+ }
+}
 
 	
